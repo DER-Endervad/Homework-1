541cd48ead71dc6a7bea515a409dc8430b911906
Даниил <derendervad@gmail.com>
2022-11-09 22:41:16 +0300

    commit initial

---
+++ Homework 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::ifstream file("C:\\C++\\Homework 1\\in.txt"); // тут напишите свой путь к файлу.
+    if (file.is_open()) {
+        while (file.peek() != EOF) {
+            int size = 0, number = 0;
+            file >> size;
+            int* arr = new int[size];
+            for (int i = (size - 1); i > -1; i--) {
+                file >> arr[i];
+            }
+            std::ofstream out("C:\\C++\\Homework 1\\out.txt", std::ios::app); // тут напишите свой путь к файлу.
+            out << size; out << '\n';
+            for (int i = 0; i < size; i++) {
+                out << arr[i];
+                out << ' ';
+            }
+            out << '\n';
+            delete[] arr;
+        }
+    }
+    else {
+        std::cout << "Файл не открылся" << std::endl;
+    }
+}
